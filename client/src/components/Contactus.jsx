941b0bafe6270c13ec8aabe1240de2619fe911cc
05-yuvraj-singh <yuvrajsingh532004@gmail.com>
2023-10-04 23:01:04 +0530

    Added more content to contact us

--- client/src/components/Contactus.jsx
+++ client/src/components/Contactus.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from 'react';
+import {
+  Box,
+  Text,
+  FormControl,
+  Link,
+  Input,
+  Textarea,
+  Button,
+} from '@chakra-ui/react';
 
-function Contactus() {
+function ContactUs() {
   return (
-    <>
-        <h2>THIS IS A CONTACT US PAGE</h2> 
-    </>
-  )
+    <Box 
+    display="flex"
+    flexDirection="column"
+    alignItems="center"
+    justifyContent="center"
+    height="60vh"
+    fontFamily="'Martian Mono', monospace, 'Mooli', sans-serif, 'Roboto', sans-serif">
+      <Text fontSize="4xl" mt={5} mb={5} >Contact Us</Text>
+
+      <Text>Contact our support team for assistance or inquiries.</Text>
+
+      <form maxW="400px">
+        <FormControl id="name" mt={4}>
+          <Input type="text" placeholder="Your Name" />
+        </FormControl>
+
+        <FormControl id="email" mt={4}>
+          <Input type="email" placeholder="Your Email Address" />
+        </FormControl>
+
+        <FormControl id="message" mt={4}>
+          <Textarea
+            placeholder="Write your message here..."
+            rows={4}
+          />
+        </FormControl>
+
+        <Button type="submit" colorScheme="blue" mt={4}>
+          Submit
+        </Button>
+      </form>
+
+      <Text mt={4}>
+        Alternatively, you can also contact the developer directly at{' '}<br/>
+        <Link href="officialyuvrajsingh05@gmail.com">GMail</Link><br/>
+        <Link href="https://github.com/05-yuvraj-singh">GitHub</Link>
+      </Text>
+
+      <Text mt={2}>
+        If you'd like to contribute to our project or report issues, please visit
+        our GitHub repository:{' '}
+        <Link href="https://github.com/05-yuvraj-singh/Yumsies2.0">
+          GitHub Repository
+        </Link>.
+      </Text>
+    </Box>
+  );
 }
 
-export default Contactus
+export default ContactUs;
