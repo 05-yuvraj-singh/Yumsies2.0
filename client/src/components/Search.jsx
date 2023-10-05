a064e9acb0e9413af98d7124b06e951afee7de40
05-yuvraj-singh <yuvrajsingh532004@gmail.com>
2023-10-05 18:53:05 +0530

    done with search component

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import {
+  Box,
+  Input,
+  VStack,
+  Heading,
+  Text,
+  Grid,
+  GridItem,
+} from '@chakra-ui/react';
 
 function Search() {
+  const [searchText, setSearchText] = useState('');
+  const [recipes, setRecipes] = useState([]);
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  useEffect(() => {
+    const API_ENDPOINT = `/api/recipes?search=${searchText}`;
+
+    fetch(API_ENDPOINT)
+      .then((response) => response.json())
+      .then((data) => {
+        setRecipes(data.recipes);
+      })
+      .catch((error) => {
+        console.error('Error fetching recipes:', error);
+      });
+  }, [searchText]);
+
   return (
-    <>
-     <h2>SEARCH COMPONENT</h2> 
-    </>
-  )
+    <Box p={4}>
+      <VStack align="center" spacing={4}>
+        <Heading as="h2" mb={4}>
+          Search Recipes
+        </Heading>
+        <Input
+          type="text"
+          placeholder="Search for recipes..."
+          value={searchText}
+          onChange={handleSearchChange}
+          maxW="400px"
+        />
+      </VStack>
+
+      {recipes.length > 0 && (
+        <Box mt={8}>
+          <Text fontSize="lg" fontWeight="bold">
+            Search Results:
+          </Text>
+          <Grid templateColumns="repeat(2, 1fr)" gap={4}>
+            {recipes.map((recipe, index) => (
+              <GridItem key={index}>
+                <Box borderWidth="1px" borderRadius="lg" p={4}>
+                  <Heading as="h3" size="md">
+                    {recipe.title}
+                  </Heading>
+                  <Text>{recipe.description}</Text>
+                </Box>
+              </GridItem>
+            ))}
+          </Grid>
+        </Box>
+      )}
+    </Box>
+  );
 }
 
-export default Search
+export default Search;
