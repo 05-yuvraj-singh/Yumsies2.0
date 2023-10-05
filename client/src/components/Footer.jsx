174cec2ae25695aa0edc5aa8caa5dc94fa39e010
05-yuvraj-singh <yuvrajsingh532004@gmail.com>
2023-10-05 16:22:16 +0530

    Hackerrank

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,35 +1,70 @@
 import React from 'react';
-import { Box, Link, Text } from "@chakra-ui/react";
+import { Box, Link, Text, Flex } from "@chakra-ui/react";
 
 function Footer() {
   const footerStyles = {
-    backgroundColor: "#ff9966", // Background color
+    backgroundColor: "#ff9966",
     fontFamily: "'Martian Mono', monospace, 'Mooli', sans-serif, 'Roboto', sans-serif",
     display: "flex",
-    justifyContent: "center", // Center content horizontally
-    alignItems: "center", // Center content vertically
+    justifyContent: "center",
+    alignItems: "center",
     height: "40vh",
   };
 
   const linkStyles = {
-    color: "white", // White color for links
-    margin: "0 10px", // Add margin between links
+    color: "white",
+    margin: "25px 30px", // Add margin between links (adjust as needed)
+  };
+
+  const boxStyles = {
+    marginRight: "20px", // Add horizontal margin between boxes
+    textAlign: "center", // Center text within each box
   };
 
   return (
     <Box style={footerStyles}>
-      <Link href="/about" style={linkStyles}>
-        About Us
-      </Link>
-      <Link href="/contact-us" style={linkStyles}>
-        Contact Us
-      </Link>
-      <Link href="https://github.com/05-yuvraj-singh/Yumsies2.0" style={linkStyles}>
-        View Code
-      </Link>
-      <Text style={{ color: "white", margin: "0 10px" }}>
-        © Copyright {new Date().getFullYear()}  Yumsies , inc. All rights reserved.
-      </Text>
+      <Flex>
+        <Box style={boxStyles}>
+          <div>
+            <Link href="/about" style={linkStyles} >
+              About Us
+            </Link>
+          </div>
+          <div>
+            <Link href="/contact-us" style={linkStyles}>
+              Contact Us
+            </Link>
+          </div>
+          <div>
+            <Link href="https://github.com/05-yuvraj-singh/Yumsies2.0" style={linkStyles}>
+              View Code
+            </Link>
+          </div>
+          <div>
+            <Text style={{ color: "white", margin: "10px 0" }}>
+              © Copyright {new Date().getFullYear()}  Yumsies, Inc. All rights reserved.
+            </Text>
+          </div>
+        </Box>
+
+        <Box style={boxStyles}>
+          <div>
+            <Link href="/about" style={linkStyles}>
+              About Us
+            </Link>
+          </div>
+          <div>
+            <Link href="/contact-us" style={linkStyles}>
+              Contact Us
+            </Link>
+          </div>
+          <div>
+            <Link href="https://github.com/05-yuvraj-singh/Yumsies2.0" style={linkStyles}>
+              View Code
+            </Link>
+          </div>
+        </Box>
+      </Flex>
     </Box>
   );
 }
